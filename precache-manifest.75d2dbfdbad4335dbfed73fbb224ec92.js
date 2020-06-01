8b2e4719aaf633f79ab36f8edf3a4b9b5a9a4fc5
Janani SriGuha <janani@blutechia.com>
2020-06-01 17:54:15 +0530

    Updates

---
+++ precache-manifest.75d2dbfdbad4335dbfed73fbb224ec92.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50773e68272de684f8b47f7b0a8c560c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c05ea2fd5c0c27532df3",
+    "url": "/monsters-rolodex/static/css/main.27f015e2.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c05ea2fd5c0c27532df3",
+    "url": "/monsters-rolodex/static/js/main.722f3586.chunk.js"
+  },
+  {
+    "revision": "6062e4a584c29c6e0372",
+    "url": "/monsters-rolodex/static/js/runtime-main.e3d252a9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
